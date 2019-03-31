9ed883e7772238f8566880c3d47c5dbc16ba8ab7
spland <xiexinyu6252@163.com>
2019-03-31 10:01:29 +0800

    git add sout

--- src/main/java/com/example/demo/controller/HelloWorld.java
+++ src/main/java/com/example/demo/controller/HelloWorld.java
@@ -3,5 +3,6 @@ package com.example.demo.controller;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("branch name is mybranch1");
     }
 }
