b2475cb7a9e74ea600f1a74eb83cf61d89742760
spland <xiexinyu6252@163.com>
2019-03-31 21:24:37 +0800

    git 回退版本需要到指定的分支上做提交

--- src/main/java/com/example/demo/controller/HelloWorld.java
+++ src/main/java/com/example/demo/controller/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
         System.out.println("branch name is mybranch1");
         System.out.println("branch test");
         System.out.println("branch finally");
+        System.out.println("只是没有到指定的分支上修改所以会出现git detached 这样类似的错误");
     }
 }
