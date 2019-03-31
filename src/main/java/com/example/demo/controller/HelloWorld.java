ef4673c835402e541d92d10fa131880794d47057
spland <xiexinyu6252@163.com>
2019-03-31 11:26:28 +0800

    git 重置了当前分支

--- src/main/java/com/example/demo/controller/HelloWorld.java
+++ src/main/java/com/example/demo/controller/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("branch name is mybranch1");
+        System.out.println("重置当前分支");
     }
 }
